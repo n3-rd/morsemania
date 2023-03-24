f9d6684bd347f24a1c0927c3a17c96d06808f35d
n3-rd <godwinjdayo@gmail.com>
2023-03-24 12:23:56 +0100

    added pages

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,1 +1,100 @@
-<h1>Signup</h1>
+<!-- simple signup form -->
+<script>
+	let email;
+	let password;
+	let name;
+
+	import { Client, Account, ID } from 'appwrite';
+
+	const client = new Client()
+		.setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) // Set only when using self-hosted solution
+		.setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // Your project ID
+
+	const createAccount = () => {
+		const account = new Account(client);
+
+		// Register User
+		account.create(ID.unique(), email, password, name).then(
+			(response) => {
+				console.log(response);
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	};
+</script>
+
+<section class="h-screen">
+	<div class="container h-full px-6 py-12">
+		<div class="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+			<div class="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
+				<img
+					src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+					class="w-full"
+					alt="Phone image"
+				/>
+			</div>
+			<div class="md:w-8/12 lg:ml-6 lg:w-5/12">
+				<form on:submit|preventDefault={createAccount}>
+					<!-- name -->
+					<div class="relative mb-6" data-te-input-wrapper-init>
+						<input
+							type="text"
+							class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+							id="exampleFormControlInput3"
+							placeholder="Email address"
+							bind:value={name}
+						/>
+						<label
+							for="exampleFormControlInput3"
+							class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none"
+							>Name
+						</label>
+					</div>
+					<!-- Email input -->
+					<div class="relative mb-6" data-te-input-wrapper-init>
+						<input
+							type="email"
+							class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+							id="exampleFormControlInput3"
+							placeholder="Email address"
+							bind:value={email}
+						/>
+						<label
+							for="exampleFormControlInput3"
+							class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none"
+							>Email address
+						</label>
+					</div>
+
+					<!-- Password input -->
+					<div class="relative mb-6" data-te-input-wrapper-init>
+						<input
+							type="password"
+							class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+							id="exampleFormControlInput33"
+							placeholder="Password"
+							bind:value={password}
+						/>
+						<label
+							for="exampleFormControlInput33"
+							class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none"
+							>Password
+						</label>
+					</div>
+
+					<!-- Submit button -->
+					<button
+						type="submit"
+						class="inline-block w-full rounded bg-black px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+						data-te-ripple-init
+						data-te-ripple-color="light"
+					>
+						Sign up
+					</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
