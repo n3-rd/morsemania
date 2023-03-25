548e631df0d6c1242d68353bf87e8f9396f107ea
n3-rd <godwinjdayo@gmail.com>
2023-03-25 10:53:24 +0100

    morse code now plays properly

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,17 +1,8 @@
 <script>
 	import { goto } from '$app/navigation';
 	import { state } from '../../store';
-	import { Howl } from 'howler';
+	import { Howl, Howler } from 'howler';
 	import { onMount } from 'svelte';
-	onMount(async () => {
-		await state.checkSession().catch(() => {
-			goto('/login');
-		});
-	});
-	const logout = async () => {
-		await state.logout();
-		goto('/login');
-	};
 
 	const codeToAudio = {
 		'.': '/sound/morse-dot.wav',
@@ -20,6 +11,7 @@
 	};
 
 	const playAudio = (src) => {
+		Howler.stop();
 		return new Promise((resolve) => {
 			const sound = new Howl({
 				src: [src],
@@ -44,10 +36,76 @@
 				await playAudio(src);
 			}
 		}
+
+		isPlaying = false; // Set isPlaying to false after the morse code is done playing
+	};
+
+	let morseCode = '';
+	let morseCodeSymbols = '';
+	let guess = '';
+	let isPlaying = false;
+
+	const codes = [
+		{ text: 'PROGRAM', code: '.--. .-. --- --. .-. --- --. .-.. .' },
+		{ text: 'SVELTE', code: '... ...- .-.. - .-' },
+		{
+			text: 'JAVASCRIPT',
+			code: '.--- .- ...- .- ...-.- -.-- .--. ..- --. ....- .. - -.-. .-.. .. -.-. -.--'
+		}
+	];
+
+	const getRandomCode = () => {
+		const index = Math.floor(Math.random() * codes.length);
+		return codes[index];
+	};
+
+	const startGame = () => {
+		const code = getRandomCode();
+		morseCode = code.text;
+		morseCodeSymbols = code.code;
+		playMorseCode(morseCodeSymbols);
+		isPlaying = true;
+	};
+
+	const submitGuess = () => {
+		const textToMorse = (text) => {
+			return text
+				.toUpperCase()
+				.split('')
+				.map((char) => codeToAudio[char])
+				.filter((src) => src !== undefined) // remove undefined sources (for spaces)
+				.join(' ');
+		};
+
+		if (guess === morseCode) {
+			// Check if the guess is correct
+			alert('Correct!');
+			isPlaying = false;
+			guess = '';
+			morseCodeSymbols = '';
+		} else {
+			alert('Incorrect. Try again.');
+		}
+	};
+
+	onMount(async () => {
+		await state.checkSession().catch(() => {
+			goto('/login');
+		});
+	});
+
+	const logout = async () => {
+		await state.logout();
+		goto('/login');
 	};
 </script>
 
-<h1>Game</h1>
+<h1>Guess the Morse Code</h1>
 <button class="logout" on:click={logout}>Logout</button>
-<!-- play morse code bbutton -->
-<button on:click={() => playMorseCode('.--. .-. --- --. .-. --- --. .-.. .')}>Play</button>
+<div>
+	<p>Morse Code: {morseCodeSymbols}</p>
+	<p>Guess: {guess}</p>
+	<input bind:value={guess} on:input={() => (guess = guess.toUpperCase())} />
+	<button on:click={startGame}>Start</button>
+	<button on:click={submitGuess}>Submit</button>
+</div>
