767f686e7ee35eb7fb63bea6fb623692638b0359
n3-rd <godwinjdayo@gmail.com>
2023-03-26 09:07:30 +0100

    added more codes

--- src/lib/morseDictionary.js
+++ src/lib/morseDictionary.js
@@ -1,10 +1,27 @@
 const codes = [
-    { text: 'PROGRAM', code: '.--. .-. --- --. .-. --- --. .-.. .' },
-    { text: 'SVELTE', code: '... ...- .-.. - .-' },
+    { text: 'PROGRAM', code: '.--. .-. --- --. .-. .- --' },
+    { text: 'SVELTE', code: '... ...- . .-.. - .' },
     {
         text: 'JAVASCRIPT',
-        code: '.--- .- ...- .- ...-.- -.-- .--. ..- --. ....- .. - -.-. .-.. .. -.-. -.--'
-    }
+        code: '.--- .- ...- .- ... -.-. .-. .. .--. -'
+    },
+    { text: 'APPWRITE', code: '.- .--. .--. .-- .-. .. - .' },
+    { text: 'NODEJS', code: '-. --- -.. . .--- ...' },
+    { text: 'RUST', code: '.-. ..- ... -' },
+    { text: 'HYDRATION', code: '.... -.-- -.. .-. .- - .. --- -.' },
+    { text: 'KIT', code: '-.- .. -' },
+    { text: 'FRAMEWORK', code: '..-. .-. .- -- . .-- --- .-. -.-' },
+    { text: 'AI', code: '.- ..' },
+    { text: 'ORACLE', code: '--- .-. .- -.-. .-.. .' },
+    { text: 'DOCKER', code: '-.. --- -.-. -.- . .-.' },
+    { text: 'GITHUB', code: '--. .. - .... ..- -...' },
+    { text: 'CONTAINER', code: '-.-. --- -. - .- .. -. . .-.' },
+    { text: 'AUTH', code: '.- ..- - ....' },
+    { text: 'DATABASE', code: '-.. .- - .- -... .- ... .' },
+    { text: 'EXPORT', code: '. -..- .--. --- .-. -' },
+    { text: 'MIDJOURNEY', code: '-- .. -.. .--- --- ..- .-. -. . -.--' },
+    { text: 'ETH', code: '. - ....' },
+    { text: 'ASTRO', code: '.- ... - .-. ---' },
 ]
 
 export default codes;
